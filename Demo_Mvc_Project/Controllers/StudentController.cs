082fec4064899f25ca1904e35acc8eeeadcd540a
nilsu <nilsu@DESKTOP-O6Q5UAT>
2022-09-04 00:42:59 +0300

    Add project files.

---
+++ Demo_Mvc_Project/Controllers/StudentController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Demo_Mvc_Project.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult StudentExamList()
+        {
+            return View();
+        }
+
+        public ActionResult LessonList()
+        {
+            return View();
+        }
+    }
+}
